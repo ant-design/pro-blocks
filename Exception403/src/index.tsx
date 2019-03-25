df1f67e33832a66e4e6d07272e41647721528911
xiaohuoni <448627663@qq.com>
2019-03-25 23:49:53 +0800

    exception

--- Exception403/src/index.tsx
+++ Exception403/src/index.tsx
@@ -1,14 +1,15 @@
-import Exception from './components/Exception';
+
 import React from 'react';
 import { formatMessage } from 'umi-plugin-react/locale';
 import Link from 'umi/link';
+import Exception from './components/Exception';
 
 const PAGE_NAME_UPPER_CAMEL_CASE: React.FC = () => (
   <Exception
     type="403"
     desc={formatMessage({ id:'BLOCK_NAME.description.403' })}
     linkElement={Link}
-    backText={formatMessage({ id: 'BLOCK_NAME.description.back' })}
+    backText={formatMessage({ id: 'BLOCK_NAME.exception.back' })}
   />
 );
 
