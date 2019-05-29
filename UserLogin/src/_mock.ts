46f69af6967d561a70ef7c5adb2d3a3a837d08c3
陈帅 <qixian.cs@outlook.com>
2019-05-29 17:56:38 +0800

    fix login url

--- UserLogin/src/_mock.ts
+++ UserLogin/src/_mock.ts
@@ -3,7 +3,7 @@ function getFakeCaptcha(req: any, res: { json: (arg0: string) => void }) {
 }
 
 export default {
-  'POST  /api/account': (
+  'POST  /api/login/account': (
     req: { body: { password: any; userName: any; type: any } },
     res: {
       send: (data: any) => void;
@@ -32,5 +32,5 @@ export default {
       currentAuthority: 'guest',
     });
   },
-  'GET  /api/captcha': getFakeCaptcha,
+  'GET  /api/login/captcha': getFakeCaptcha,
 };
