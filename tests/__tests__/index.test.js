d57228a6513bb22cbb8c0659027d0f1556cbdc9d
陈帅 <qixian.cs@outlook.com>
2019-07-08 18:53:27 +0800

    add test

---
+++ tests/__tests__/index.test.js
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const diffImage = require('block-screenshot/src/screenshot');
+const path = require('path');
+const { winPath } = require('umi-utils');
+
+jest.setTimeout(50000000);
+describe('Pro Block', () => {
+  it('🥩 diff all block images', async () => {
+    const diffFileList = await diffImage({
+      cwd: path.join(winPath(__dirname), '../../'),
+      diff: true,
+    });
+    expect(diffFileList).toBe(0);
+  });
+});
