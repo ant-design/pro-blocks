9cc3fd1e2693ca4f849f1e9644ad2f9767fb2ac6
陈帅 <qixian.cs@outlook.com>
2019-10-11 17:18:13 +0800

    better emptyPage  code

--- EmptyPage/src/index.tsx
+++ EmptyPage/src/index.tsx
@@ -1,6 +1,7 @@
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import React, { useState, useEffect } from 'react';
 import { Spin } from 'antd';
+import styles from './index.less';
 
 export default () => {
   const [loading, setLoading] = useState<boolean>(true);
@@ -10,7 +11,7 @@ export default () => {
     }, 3000);
   }, []);
   return (
-    <PageHeaderWrapper content="这是一个新页面，从这里进行开发！">
+    <PageHeaderWrapper content="这是一个新页面，从这里进行开发！" className={styles.main}>
       <div style={{ paddingTop: 100, textAlign: 'center' }}>
         <Spin spinning={loading} size="large"></Spin>
       </div>
