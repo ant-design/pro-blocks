fafdf8d7a886c4fa23d673047e2eebe5ed720221
chenshuai2144 <qixian.cs@outlook.com>
2019-12-06 18:27:00 +0800

    fix login no work bug

--- UserLogin/src/utils/utils.ts
+++ UserLogin/src/utils/utils.ts
@@ -6,5 +6,16 @@ export function getPageQuery() {
 
 export function setAuthority(authority: string | string[]) {
   const proAuthority = typeof authority === 'string' ? [authority] : authority;
-  return localStorage.setItem('antd-pro-authority', JSON.stringify(proAuthority));
+  localStorage.setItem('antd-pro-authority', JSON.stringify(proAuthority));
+  // hard code
+  // reload Authorized component
+  try {
+    if ((window as any).reloadAuthorized) {
+      (window as any).reloadAuthorized();
+    }
+  } catch (error) {
+    // do not need do anything
+  }
+
+  return authority;
 }
