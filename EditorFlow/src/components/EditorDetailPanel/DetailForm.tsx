c66fbafb9744e244f38e2ab23777e47116735d8d
wangxingkang <wangxingkang@sensoro.com>
2020-01-14 15:11:54 +0800

    feat: 代码优化

--- EditorFlow/src/components/EditorDetailPanel/DetailForm.tsx
+++ EditorFlow/src/components/EditorDetailPanel/DetailForm.tsx
@@ -33,11 +33,7 @@ class DetailForm extends React.Component<DetailFormProps> {
     return propsAPI.getSelected()[0];
   }
 
-  handleSubmit = (e?: React.FormEvent) => {
-    if (e && e.preventDefault) {
-      e.preventDefault();
-    }
-
+  handleSubmit = () => {
     const { form, propsAPI } = this.props;
     const { getSelected, executeCommand, update } = propsAPI;
 
@@ -63,7 +59,7 @@ class DetailForm extends React.Component<DetailFormProps> {
   };
 
   renderEdgeShapeSelect = () => (
-    <Select onChange={() => { this.handleSubmit() }}>
+    <Select onChange={this.handleSubmit}>
       <Option value="flow-smooth">Smooth</Option>
       <Option value="flow-polyline">Polyline</Option>
       <Option value="flow-polyline-round">Polyline Round</Option>
