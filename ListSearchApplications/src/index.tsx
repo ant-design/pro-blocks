03f58d860149abb66c91591cc006af51d419b701
wangxingkang <wangxingkang@sensoro.com>
2020-01-19 18:06:28 +0800

    feat: ListSearchApplications Feature migrate antd@4

--- ListSearchApplications/src/index.tsx
+++ ListSearchApplications/src/index.tsx
@@ -4,13 +4,10 @@ import {
   EllipsisOutlined,
   ShareAltOutlined,
 } from '@ant-design/icons';
-import { Form } from '@ant-design/compatible';
-import '@ant-design/compatible/assets/index.css';
-import { Avatar, Card, Col, Dropdown, List, Menu, Row, Select, Tooltip } from 'antd';
-import React, { Component } from 'react';
+import { Avatar, Card, Col, Dropdown, List, Menu, Row, Select, Tooltip, Form } from 'antd';
+import React, { FC, useEffect } from 'react';
 
 import { Dispatch } from 'redux';
-import { FormComponentProps } from '@ant-design/compatible/es/form';
 import { connect } from 'dva';
 import numeral from 'numeral';
 import { StateType } from './model';
@@ -20,7 +17,6 @@ import TagSelect from './components/TagSelect';
 import styles from './style.less';
 
 const { Option } = Select;
-const FormItem = Form.Item;
 
 export function formatWan(val: number) {
   const v = val * 1;
@@ -48,183 +44,163 @@ export function formatWan(val: number) {
   return result;
 }
 
-interface PAGE_NAME_UPPER_CAMEL_CASEProps extends FormComponentProps {
+interface PAGE_NAME_UPPER_CAMEL_CASEProps {
   dispatch: Dispatch<any>;
   BLOCK_NAME_CAMEL_CASE: StateType;
   loading: boolean;
 }
 
-class PAGE_NAME_UPPER_CAMEL_CASE extends Component<PAGE_NAME_UPPER_CAMEL_CASEProps> {
-  componentDidMount() {
-    const { dispatch } = this.props;
-    dispatch({
-      type: 'BLOCK_NAME_CAMEL_CASE/fetch',
-      payload: {
-        count: 8,
-      },
-    });
-  }
-
-  render() {
-    const {
-      BLOCK_NAME_CAMEL_CASE: { list },
-      loading,
-      form,
-    } = this.props;
-    const { getFieldDecorator } = form;
-
-    const CardInfo: React.FC<{
-      activeUser: React.ReactNode;
-      newUser: React.ReactNode;
-    }> = ({ activeUser, newUser }) => (
-      <div className={styles.cardInfo}>
-        <div>
-          <p>活跃用户</p>
-          <p>{activeUser}</p>
-        </div>
-        <div>
-          <p>新增用户</p>
-          <p>{newUser}</p>
-        </div>
-      </div>
-    );
-
-    const formItemLayout = {
-      wrapperCol: {
-        xs: { span: 24 },
-        sm: { span: 16 },
-      },
-    };
-
-    const itemMenu = (
-      <Menu>
-        <Menu.Item>
-          <a target="_blank" rel="noopener noreferrer" href="https://www.alipay.com/">
-            1st menu item
-          </a>
-        </Menu.Item>
-        <Menu.Item>
-          <a target="_blank" rel="noopener noreferrer" href="https://www.taobao.com/">
-            2nd menu item
-          </a>
-        </Menu.Item>
-        <Menu.Item>
-          <a target="_blank" rel="noopener noreferrer" href="https://www.tmall.com/">
-            3d menu item
-          </a>
-        </Menu.Item>
-      </Menu>
-    );
-
-    return (
-      <div className={styles.filterCardList}>
-        <Card bordered={false}>
-          <Form layout="inline">
-            <StandardFormRow title="所属类目" block style={{ paddingBottom: 11 }}>
-              <FormItem>
-                {getFieldDecorator('category')(
-                  <TagSelect expandable>
-                    <TagSelect.Option value="cat1">类目一</TagSelect.Option>
-                    <TagSelect.Option value="cat2">类目二</TagSelect.Option>
-                    <TagSelect.Option value="cat3">类目三</TagSelect.Option>
-                    <TagSelect.Option value="cat4">类目四</TagSelect.Option>
-                    <TagSelect.Option value="cat5">类目五</TagSelect.Option>
-                    <TagSelect.Option value="cat6">类目六</TagSelect.Option>
-                    <TagSelect.Option value="cat7">类目七</TagSelect.Option>
-                    <TagSelect.Option value="cat8">类目八</TagSelect.Option>
-                    <TagSelect.Option value="cat9">类目九</TagSelect.Option>
-                    <TagSelect.Option value="cat10">类目十</TagSelect.Option>
-                    <TagSelect.Option value="cat11">类目十一</TagSelect.Option>
-                    <TagSelect.Option value="cat12">类目十二</TagSelect.Option>
-                  </TagSelect>,
-                )}
-              </FormItem>
-            </StandardFormRow>
-            <StandardFormRow title="其它选项" grid last>
-              <Row gutter={16}>
-                <Col lg={8} md={10} sm={10} xs={24}>
-                  <FormItem {...formItemLayout} label="作者">
-                    {getFieldDecorator(
-                      'author',
-                      {},
-                    )(
-                      <Select placeholder="不限" style={{ maxWidth: 200, width: '100%' }}>
-                        <Option value="lisa">王昭君</Option>
-                      </Select>,
-                    )}
-                  </FormItem>
-                </Col>
-                <Col lg={8} md={10} sm={10} xs={24}>
-                  <FormItem {...formItemLayout} label="好评度">
-                    {getFieldDecorator(
-                      'rate',
-                      {},
-                    )(
-                      <Select placeholder="不限" style={{ maxWidth: 200, width: '100%' }}>
-                        <Option value="good">优秀</Option>
-                        <Option value="normal">普通</Option>
-                      </Select>,
-                    )}
-                  </FormItem>
-                </Col>
-              </Row>
-            </StandardFormRow>
-          </Form>
-        </Card>
-        <br />
-        <List<ListItemDataType>
-          rowKey="id"
-          grid={{ gutter: 24, xl: 4, lg: 3, md: 3, sm: 2, xs: 1 }}
-          loading={loading}
-          dataSource={list}
-          renderItem={item => (
-            <List.Item key={item.id}>
-              <Card
-                hoverable
-                bodyStyle={{ paddingBottom: 20 }}
-                actions={[
-                  <Tooltip key="download" title="下载">
-                    <DownloadOutlined />
-                  </Tooltip>,
-                  <Tooltip key="edit" title="编辑">
-                    <EditOutlined />
-                  </Tooltip>,
-                  <Tooltip title="分享" key="share">
-                    <ShareAltOutlined />
-                  </Tooltip>,
-                  <Dropdown key="ellipsis" overlay={itemMenu}>
-                    <EllipsisOutlined />
-                  </Dropdown>,
-                ]}
-              >
-                <Card.Meta avatar={<Avatar size="small" src={item.avatar} />} title={item.title} />
-                <div className={styles.cardItemContent}>
-                  <CardInfo
-                    activeUser={formatWan(item.activeUser)}
-                    newUser={numeral(item.newUser).format('0,0')}
-                  />
-                </div>
-              </Card>
-            </List.Item>
-          )}
-        />
-      </div>
-    );
-  }
-}
-
-const WarpForm = Form.create<PAGE_NAME_UPPER_CAMEL_CASEProps>({
-  onValuesChange({ dispatch }: PAGE_NAME_UPPER_CAMEL_CASEProps) {
-    // 表单项变化时请求数据
-    // 模拟查询表单生效
-    dispatch({
-      type: 'BLOCK_NAME_CAMEL_CASE/fetch',
-      payload: {
-        count: 8,
-      },
-    });
+const formItemLayout = {
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 },
   },
-})(PAGE_NAME_UPPER_CAMEL_CASE);
+};
+
+const CardInfo: React.FC<{
+  activeUser: React.ReactNode;
+  newUser: React.ReactNode;
+}> = ({ activeUser, newUser }) => (
+  <div className={styles.cardInfo}>
+    <div>
+      <p>活跃用户</p>
+      <p>{activeUser}</p>
+    </div>
+    <div>
+      <p>新增用户</p>
+      <p>{newUser}</p>
+    </div>
+  </div>
+);
+
+export const PAGE_NAME_UPPER_CAMEL_CASE: FC<PAGE_NAME_UPPER_CAMEL_CASEProps> = props => {
+  const {
+    dispatch,
+    loading,
+    BLOCK_NAME_CAMEL_CASE: { list },
+  } = props;
+
+  useEffect(() => {
+    dispatch({
+      type: 'BLOCK_NAME_CAMEL_CASE/fetch',
+      payload: {
+        count: 8,
+      },
+    });
+  }, [1]);
+
+  const handleValuesChange = () => {
+    dispatch({
+      type: 'BLOCK_NAME_CAMEL_CASE/fetch',
+      payload: {
+        count: 8,
+      },
+    });
+  };
+
+  const itemMenu = (
+    <Menu>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.alipay.com/">
+          1st menu item
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.taobao.com/">
+          2nd menu item
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.tmall.com/">
+          3d menu item
+        </a>
+      </Menu.Item>
+    </Menu>
+  );
+
+  return (
+    <div className={styles.filterCardList}>
+      <Card bordered={false}>
+        <Form onValuesChange={handleValuesChange}>
+          <StandardFormRow title="所属类目" block style={{ paddingBottom: 11 }}>
+            <Form.Item name="category">
+              <TagSelect expandable>
+                <TagSelect.Option value="cat1">类目一</TagSelect.Option>
+                <TagSelect.Option value="cat2">类目二</TagSelect.Option>
+                <TagSelect.Option value="cat3">类目三</TagSelect.Option>
+                <TagSelect.Option value="cat4">类目四</TagSelect.Option>
+                <TagSelect.Option value="cat5">类目五</TagSelect.Option>
+                <TagSelect.Option value="cat6">类目六</TagSelect.Option>
+                <TagSelect.Option value="cat7">类目七</TagSelect.Option>
+                <TagSelect.Option value="cat8">类目八</TagSelect.Option>
+                <TagSelect.Option value="cat9">类目九</TagSelect.Option>
+                <TagSelect.Option value="cat10">类目十</TagSelect.Option>
+                <TagSelect.Option value="cat11">类目十一</TagSelect.Option>
+                <TagSelect.Option value="cat12">类目十二</TagSelect.Option>
+              </TagSelect>
+            </Form.Item>
+          </StandardFormRow>
+          <StandardFormRow title="其它选项" grid last>
+            <Row gutter={16}>
+              <Col lg={8} md={10} sm={10} xs={24}>
+                <Form.Item {...formItemLayout} name="author" label="作者">
+                  <Select placeholder="不限" style={{ maxWidth: 200, width: '100%' }}>
+                    <Option value="lisa">王昭君</Option>
+                  </Select>
+                </Form.Item>
+              </Col>
+              <Col lg={8} md={10} sm={10} xs={24}>
+                <Form.Item {...formItemLayout} name="rate" label="好评度">
+                  <Select placeholder="不限" style={{ maxWidth: 200, width: '100%' }}>
+                    <Option value="good">优秀</Option>
+                    <Option value="normal">普通</Option>
+                  </Select>
+                </Form.Item>
+              </Col>
+            </Row>
+          </StandardFormRow>
+        </Form>
+      </Card>
+      <br />
+      <List<ListItemDataType>
+        rowKey="id"
+        grid={{ gutter: 24, xl: 4, lg: 3, md: 3, sm: 2, xs: 1 }}
+        loading={loading}
+        dataSource={list}
+        renderItem={item => (
+          <List.Item key={item.id}>
+            <Card
+              hoverable
+              bodyStyle={{ paddingBottom: 20 }}
+              actions={[
+                <Tooltip key="download" title="下载">
+                  <DownloadOutlined />
+                </Tooltip>,
+                <Tooltip key="edit" title="编辑">
+                  <EditOutlined />
+                </Tooltip>,
+                <Tooltip title="分享" key="share">
+                  <ShareAltOutlined />
+                </Tooltip>,
+                <Dropdown key="ellipsis" overlay={itemMenu}>
+                  <EllipsisOutlined />
+                </Dropdown>,
+              ]}
+            >
+              <Card.Meta avatar={<Avatar size="small" src={item.avatar} />} title={item.title} />
+              <div className={styles.cardItemContent}>
+                <CardInfo
+                  activeUser={formatWan(item.activeUser)}
+                  newUser={numeral(item.newUser).format('0,0')}
+                />
+              </div>
+            </Card>
+          </List.Item>
+        )}
+      />
+    </div>
+  );
+};
 
 export default connect(
   ({
@@ -237,4 +213,4 @@ export default connect(
     BLOCK_NAME_CAMEL_CASE,
     loading: loading.models.BLOCK_NAME_CAMEL_CASE,
   }),
-)(WarpForm);
+)(PAGE_NAME_UPPER_CAMEL_CASE);
