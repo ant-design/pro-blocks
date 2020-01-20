77f5f76c4bac2eaa3eb6e1b4de17bba2f4906a8d
ShawDanon <shawdanon@qq.com>
2020-01-20 17:51:23 +0800

    fix: 抖动问题

--- UserRegister/src/index.tsx
+++ UserRegister/src/index.tsx
@@ -210,6 +210,7 @@ const PAGE_NAME_UPPER_CAMEL_CASE: FC<PAGE_NAME_UPPER_CAMEL_CASEProps> = ({
         >
           <FormItem
             name="password"
+            className={styles.password}
             rules={[
               {
                 validator: checkPassword,
