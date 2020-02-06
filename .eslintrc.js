aeeb5df2e703225668cfdc6154e4418f7ef72533
xiaohuoni <448627663@qq.com>
2020-02-06 17:13:43 +0800

    chore: update @umijs/fabric

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,10 +9,6 @@ module.exports = {
     'import/no-extraneous-dependencies': 0,
     '@typescript-eslint/no-explicit-any': 0,
     'import/no-unresolved': 0,
-    'react/forbid-prop-types': 0,
-    'react/jsx-props-no-spreading': 0,
-    'react/state-in-constructor': 0,
-    'react/static-property-placement': 0,
   },
   globals: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
