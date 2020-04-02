06d9d2eeac756441e3389fcc99bd1c20aad0d1ef
xiaohuoni <448627663@qq.com>
2020-04-02 10:57:33 +0800

    fix: umi router no found

--- ListSearch/src/index.tsx
+++ ListSearch/src/index.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import { Input } from 'antd';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import { connect, router } from 'umi';
+import { connect, history } from 'umi';
 
 interface PAGE_NAME_UPPER_CAMEL_CASEProps {
   match: {
@@ -20,13 +20,13 @@ class PAGE_NAME_UPPER_CAMEL_CASE extends Component<PAGE_NAME_UPPER_CAMEL_CASEPro
     const url = match.url === '/' ? '' : match.url;
     switch (key) {
       case 'articles':
-        router.push(`${url}/articles`);
+        history.push(`${url}/articles`);
         break;
       case 'applications':
-        router.push(`${url}/applications`);
+        history.push(`${url}/applications`);
         break;
       case 'projects':
-        router.push(`${url}/projects`);
+        history.push(`${url}/projects`);
         break;
       default:
         break;
