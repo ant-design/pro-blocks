b28720d02d5642b3050362c0361587eb95d5dcf7
zhangyanling <18783226594@163.com>
2020-04-20 14:13:05 +0800

    feat: ResultFail & ResultSuccess to umi3

--- ResultFail/src/index.tsx
+++ ResultFail/src/index.tsx
@@ -1,6 +1,5 @@
 import { CloseCircleOutlined, RightOutlined } from '@ant-design/icons';
 import { Button, Card, Result } from 'antd';
-import { FormattedMessage, formatMessage } from 'umi';
 import React, { Fragment } from 'react';
 
 import { GridContent } from '@ant-design/pro-layout';
@@ -9,30 +8,21 @@ import styles from './index.less';
 const Content = (
   <Fragment>
     <div className={styles.title}>
-      <FormattedMessage
-        id="BLOCK_NAME.error.hint-title"
-        defaultMessage="The content you submitted has the following error:"
-      />
+      <span>您提交的内容有如下错误：</span>
     </div>
     <div style={{ marginBottom: 16 }}>
       <CloseCircleOutlined style={{ marginRight: 8 }} className={styles.error_icon} />
-      <FormattedMessage
-        id="BLOCK_NAME.error.hint-text1"
-        defaultMessage="Your account has been frozen"
-      />
+      <span>您的账户已被冻结</span>
       <a style={{ marginLeft: 16 }}>
-        <FormattedMessage id="BLOCK_NAME.error.hint-btn1" defaultMessage="Thaw immediately" />
+        <span>立即解冻</span>
         <RightOutlined />
       </a>
     </div>
     <div>
       <CloseCircleOutlined style={{ marginRight: 8 }} className={styles.error_icon} />
-      <FormattedMessage
-        id="BLOCK_NAME.error.hint-text2"
-        defaultMessage="Your account is not yet eligible to apply"
-      />
+      <span>您的账户还不具备申请资格</span>
       <a style={{ marginLeft: 16 }}>
-        <FormattedMessage id="BLOCK_NAME.error.hint-btn2" defaultMessage="Upgrade immediately" />
+        <span>立即升级</span>
         <RightOutlined />
       </a>
     </div>
@@ -44,11 +34,11 @@ export default () => (
     <Card bordered={false}>
       <Result
         status="error"
-        title={formatMessage({ id: 'BLOCK_NAME.error.title' })}
-        subTitle={formatMessage({ id: 'BLOCK_NAME.error.description' })}
+        title="提交失败"
+        subTitle="请核对并修改以下信息后，再重新提交。"
         extra={
           <Button type="primary">
-            <FormattedMessage id="BLOCK_NAME.error.btn-text" defaultMessage="Return to modify" />
+            <span>返回修改</span>
           </Button>
         }
         style={{ marginTop: 48, marginBottom: 16 }}
