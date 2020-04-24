5a6325e329b444423b388bbacc507892ee62921d
kang.ding <kang.ding@abssqr.com>
2020-04-25 01:31:25 +0800

    refactor: ListCardList

--- ListCardList/src/service.ts
+++ ListCardList/src/service.ts
@@ -1,7 +1,10 @@
 import request from 'umi-request';
+import { CardListItemDataType } from './data.d';
 
-export async function queryFakeList(params: { count: number }) {
-  return request('/api/fake_list', {
+export async function queryFakeList(params: {
+  count: number;
+}): Promise<{ data: { list: CardListItemDataType[] } }> {
+  return request('/api/card_fake_list', {
     params,
   });
 }
