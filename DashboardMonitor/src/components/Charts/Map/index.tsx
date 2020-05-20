fb3a8f70e4bfd5d8a7aa6dd43972a0288c3e1d73
chenshuai2144 <qixian.cs@outlook.com>
2020-05-20 11:23:22 +0800

    💄 UI: better loading style

--- DashboardMonitor/src/components/Charts/Map/index.tsx
+++ DashboardMonitor/src/components/Charts/Map/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { HeatmapLayer, MapboxScene, PointLayer } from '@antv/l7-react';
-import { Spin } from 'antd';
+import { PageLoading } from '@ant-design/pro-layout';
 
 const colors = ['#eff3ff', '#c6dbef', '#9ecae1', '#6baed6', '#4292c6', '#2171b5', '#084594'];
 export default class Map extends React.Component {
@@ -29,7 +29,7 @@ export default class Map extends React.Component {
   public render() {
     const { data, grid, loading } = this.state;
     return loading === false ? (
-      <Spin />
+      <PageLoading />
     ) : (
       <MapboxScene
         map={{
