6d5eb313baf0d49b6935b10b45106d0661ca7c41
偏右 <afc163@gmail.com>
2020-06-12 18:45:16 +0800

    fix Cannot find module './data'

--- ListTableList/src/_mock.ts
+++ ListTableList/src/_mock.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Request, Response } from 'express';
 import { parse } from 'url';
-import { TableListItem, TableListParams } from './data';
+import { TableListItem, TableListParams } from './data.d';
 
 // mock tableListDataSource
 const genList = (current: number, pageSize: number) => {
