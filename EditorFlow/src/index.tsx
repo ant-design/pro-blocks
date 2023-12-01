438045b8faae1592a539aadca714306bcbcb2e37
xiaohuoni <xiaohuoni@dingtalk.com>
2023-12-01 16:25:39 +0800

    feat: use pro-components

--- EditorFlow/src/index.tsx
+++ EditorFlow/src/index.tsx
@@ -1,7 +1,7 @@
 import { Col, Row } from 'antd';
 import GGEditor, { Flow } from 'gg-editor';
 
-import { PageContainer } from '@ant-design/pro-layout';
+import { PageContainer } from '@ant-design/pro-components';
 import EditorMinimap from './components/EditorMinimap';
 import { FlowContextMenu } from './components/EditorContextMenu';
 import { FlowDetailPanel } from './components/EditorDetailPanel';
